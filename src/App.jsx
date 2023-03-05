a88a6ff56c9ebcddb8b75b5b3c25579a66c19a40
Ayush Rai <ayushrai420559@gmail.com>
2023-03-05 20:34:17 +0000

    deleted:    public/Octocat.png 	deleted:    public/favicon.ico 	modified:   public/index.html 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    public/manifest.json 	deleted:    public/robots.txt 	deleted:    src/App.css 	deleted:    src/App.js 	new file:   src/App.jsx 	deleted:    src/App.test.js 	new file:   src/components/about/About.css 	new file:   src/components/about/About.jsx 	new file:   src/components/contact/Contact.jsx 	new file:   src/components/contact/contact.css 	new file:   src/components/experience/Experience.css 	new file:   src/components/experience/Experience.jsx 	new file:   src/components/footer/Footer.css 	new file:   src/components/footer/Footer.jsx 	new file:   src/components/header/Header.css 	new file:   src/components/header/Header.jsx 	new file:   src/components/navbar/Navbar.css 	new file:   src/components/navbar/Navbar.jsx 	new file:   src/components/projects/Projects.css 	new file:   src/components/projects/Projects.jsx 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Navbar from './components/navbar/Navbar'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Projects from './components/projects/Projects'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+const App = () => {
+    return (
+        <>
+        <Header/>
+        <Navbar/>
+        <About/>
+        <Experience/>
+        <services/>
+        <Projects/>
+        <Contact/>
+        <Footer/>
+        </>
+    )
+}
+
+export default App
+
