201c97da1e43cfd5bd13c16f6456944a288680d9
Ashtorments <ayushrai420559@gmail.com>
2023-03-10 21:06:10 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.scss 	new file:   src/components/header/Header.css 	modified:   src/components/header/Header.jsx 	modified:   src/components/header/Header.scss 	new file:   src/components/header/Socials.jsx 	modified:   src/index.css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import './Header.scss';
+import './Header.css';
 import Buttons from './Buttons';
+import Mypicture from '../../assets/Mypicture.jpeg';
+import Socials from './Socials';
 // import { Link } from 'react-router-dom';
 // import '../../App.scss';
 // import '../../index.css';
 
 const Header = () => {
     return (
+        <header>
             <div className="container header-container">
                 <div className="text-zone">
                     <h1>Hi, <br/> I'm 
@@ -16,9 +19,16 @@ const Header = () => {
                     </h1>
                     <h2>Frontend Developer </h2>
                     <Buttons/>
+                    <Socials/>
+                    <div className="mypicture">
+                        <img src={Mypicture} alt="mypicture"/>
+                    </div>
+
+                    <a href="#contact" className='scroll-down'>Scroll Down</a>
                     {/* <Link to="/contact" className='flat-button'>Contact Me!</Link> */}
                 </div>
             </div>
+            </header>
     )
 }
 
