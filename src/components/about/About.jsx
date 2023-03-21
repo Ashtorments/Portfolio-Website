cb28438667e9bd25e14c86c850418d154b87b74e
Ashtorments <ayushrai420559@gmail.com>
2023-03-21 14:56:01 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/about/About.css 	modified:   src/components/about/About.jsx 	new file:   src/variants.js

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import './About.css'
 import GradPic4 from '../../assets/GradPic4.jpg'
-import { useRef, useEffect } from "react";
 import CountUp from 'react-countup';
+import { motion } from "framer-motion"
+import {fadeIn} from '../../variants'
 import { useInView } from 'react-intersection-observer';
 
 const About=()=> {
@@ -15,31 +16,41 @@ const About=()=> {
             <h2>About Me</h2>
 
             <div className="container about_container">
-                <div className="about_me">
+                <motion.div 
+                variants={fadeIn('right',0.7)}
+                initial='hidden'
+                whileInView={'show'}
+                viewport={{once:false, amount:0.5}}
+                className="about_me">
                     <div className="about_me-image">
                         <img src={GradPic4} alt=""/>
                     </div>
-                </div>
+                </motion.div>
 
-                <div className="about_content">
+                <motion.div 
+                variants={fadeIn('left',0.5)}
+                initial='hidden'
+                whileInView={'show'}
+                viewport={{once:false, amount:0.5}}
+                className="about_content">
                     <div className="about_cards">
                         <article className='about_card'>
                             <div className='about_count'>
-                            {inView  ? <CountUp start={0} end= {1} duration={3} /> : null}
+                            {inView  ? <CountUp start={0} end= {1} duration={5} /> : null}
                             </div>
                             <h6>Year of</h6>
                             <h5>Experience</h5>
                         </article>
                         <article className='about_card'>
                             <div className='about_count'>
-                            {inView ? <CountUp start={0} end= {10} duration={3}/> : null}
+                            {inView ? <CountUp start={0} end= {10} duration={5}/> : null}
                             </div>
                             <h5>Projects</h5>
                             <h6>Completed</h6>
                         </article>
                         <article className='about_card'>
                             <div className='about_count'>
-                            {inView ? <CountUp start={0} end= {8} duration={3}/> : null}
+                            {inView ? <CountUp start={0} end= {8} duration={5}/> : null}
                             </div>
                             <h5>Happy Clients</h5>
                         </article>
@@ -49,7 +60,7 @@ const About=()=> {
                         <br/>One of my favorite aspects of web development is being creative with styling, constantly tweaking and exploring new concepts, libraries, and frameworks. Moving forward, I am excited to work alongside professionals and continue growing both personally and professionally.</p>
 
                     <a href="#contact" className='btn btn-primary'>Let's Talk</a>
-                </div>
+                </motion.div>
             </div>
         </section>
     );
