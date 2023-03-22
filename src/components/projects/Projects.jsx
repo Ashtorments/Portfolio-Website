b6844d8b51d3d3aa53cc54b7e3f240ecd370e19a
Ashtorments <ayushrai420559@gmail.com>
2023-03-22 23:53:17 +0530

    new file:   src/assets/LoginAuth.jpg 	new file:   src/assets/PDFMerger.jpg 	modified:   src/components/projects/Projects.jsx

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Projects.css'
 import WeatherApp from '../../assets/WeatherApp.jpg'
+import LoginAuth from '../../assets/LoginAuth.jpg'
 
 const data = [
   {
@@ -13,36 +14,57 @@ const data = [
   {
     id: 2,
     image: WeatherApp,
-    title: "Live Weather App",
-    github: 'https://github.com/Ashtorments/Weather-App',
-    demo: 'https://ashtorments.github.io/Weather-App/'
+    title: "Portfolio Website-React",
+    github: 'https://github.com/Ashtorments/Portfolio-Website',
+    demo: '#'
   },
   {
     id: 3,
     image: WeatherApp,
-    title: "Live Weather App",
-    github: 'https://github.com/Ashtorments/Weather-App',
-    demo: 'https://ashtorments.github.io/Weather-App/'
+    title: "PDF Merger Tool",
+    github: 'https://github.com/Ashtorments/PDF-Merger-Tool',
+    demo: ''
   },
   {
     id: 4,
-    image: WeatherApp,
-    title: "Live Weather App",
-    github: 'https://github.com/Ashtorments/Weather-App',
-    demo: 'https://ashtorments.github.io/Weather-App/'
+    image: LoginAuth,
+    title: "Login Auth-Angular-Dotnet",
+    github: 'https://github.com/Ashtorments/LoginAuth-Angular-dotnet',
+    demo: ''
   },
   {
     id: 5,
     image: WeatherApp,
-    title: "Live Weather App",
-    github: 'https://github.com/Ashtorments/Weather-App',
-    demo: 'https://ashtorments.github.io/Weather-App/'
+    title: "E-mail Centre-Angular",
+    github: 'https://github.com/Ashtorments/Email-center-Angular-',
+    demo: ''
   },
   {
     id: 6,
     image: WeatherApp,
-    title: "Live Weather App",
+    title: "Coding Contest",
+    github: 'https://github.com/Ashtorments/Coding-Contests-BootStrap-',
+    demo: 'https://github.com/Ashtorments/Coding-Contests-BootStrap-'
+  },
+  {
+    id: 7,
+    image: WeatherApp,
+    title: "Calculator",
     github: 'https://github.com/Ashtorments/Weather-App',
+    demo: 'https://github.com/Ashtorments/Basic-Calculator'
+  },
+  {
+    id: 8,
+    image: WeatherApp,
+    title: "Rock Paper Scissor",
+    github: 'https://github.com/Ashtorments/Rock-Paper-Scissors-Game',
+    demo: 'https://ashtorments.github.io/Weather-App/'
+  },
+  {
+    id: 9,
+    image: WeatherApp,
+    title: "Password Generator",
+    github: 'https://github.com/Ashtorments/Password-Generator-using-JS',
     demo: 'https://ashtorments.github.io/Weather-App/'
   }
 ]
@@ -62,7 +84,7 @@ const Projects = () => {
           </div>
           <h3>{title}</h3>
           <div className="projects_item-cta">
-          <a href={github} className='btn'>Github</a>
+          <a href={github} className='btn' target='_blank'>Github</a>
           <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
           </div>
         </article>
